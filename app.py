7c7936fbc87067ca910a26f7483a3b1b04488bd5
sanjiv-24 <m.sanjivsekaran24@gmail.com>
2026-05-09 12:48:49 +0530

    Initial predictive maintenance project

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+
     return {
-        "message": "Predictive Maintenance API Running Successfully"
+        "message": "Predictive Maintenance API Running"
     }
 
 @app.get("/predict")
 def predict():
 
     predicted_rul = random.randint(10, 100)
+
+    # Better health logic
+    engine_health = min(100, predicted_rul)
 
     if predicted_rul < 20:
         status = "CRITICAL"
@@ -22,7 +26,7 @@ def predict():
         status = "HEALTHY"
 
     return {
-        "engine_health": 100 - predicted_rul,
+        "engine_health": engine_health,
         "predicted_rul": predicted_rul,
         "status": status,
         "anomaly_score": round(random.uniform(0,1),2)
